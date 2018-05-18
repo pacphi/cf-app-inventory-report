51cd60d80bd93eaf607abc1ec91662a6ba9c239c
Chris Phillipson <cphillipson@pivotal.io>
2018-05-18 07:00:16 -0700

    Lombokify

--- src/main/java/io/pivotal/cfapp/config/MailSettings.java
+++ src/main/java/io/pivotal/cfapp/config/MailSettings.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.Data;
+
+@Data
 @Configuration
 @ConfigurationProperties(prefix="spring.mail")
 public class MailSettings {
@@ -12,29 +15,5 @@ public class MailSettings {
     private String username;
     private List<String> recipients;
     private String subject;
-
-    public String getUsername() {
-        return username;
-    }
-    
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    
-    public List<String> getRecipients() {
-        return recipients;
-    }
-
-    public void setRecipients(List<String> recipients) {
-        this.recipients = recipients;
-    }
-    
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
     
 }
