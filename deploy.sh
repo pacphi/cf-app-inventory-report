2e31b1c07de5a369945282b3032c088d2753edac
Chris Phillipson <cphillipson@pivotal.io>
2018-08-09 17:11:57 -0500

    Update sh scripts

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,12 @@ fi
 
 cf login -a $1
 
-cf push get-app-details-task --no-route --health-check-type none -p ./build/libs/cf-get-app-details-0.0.1-SNAPSHOT.jar -m 1G --no-start
-cf set-env get-app-details-task SPRING_PROFILES_ACTIVE jdbc
-cf start get-app-details-task
-cf run-task get-app-details-task ".java-buildpack/open_jdk_jre/bin/java org.springframework.boot.loader.JarLauncher"
-cf create-service scheduler-for-pcf standard get-app-details-job
-cf bind-service get-app-details-task get-app-details-job
-cf create-job get-app-details-task get-app-details-scheduled-job ".java-buildpack/open_jdk_jre/bin/java org.springframework.boot.loader.JarLauncher"
-cf run-job get-app-details-scheduled-job
-cf schedule-job get-app-details-scheduled-job "0 8 ? * * "
+cf push get-app-inventory-task --no-route --health-check-type none -p ./build/libs/cf-app-inventory-report-0.1-SNAPSHOT.jar -m 1G --no-start
+cf set-env get-app-inventory-task SPRING_PROFILES_ACTIVE jdbc
+cf start get-app-inventory-task
+cf run-task get-app-inventory-task ".java-buildpack/open_jdk_jre/bin/java org.springframework.boot.loader.JarLauncher"
+cf create-service scheduler-for-pcf standard get-app-inventory-job
+cf bind-service get-app-inventory-task get-app-inventory-job
+cf create-job get-app-inventory-task get-app-inventory-scheduled-job ".java-buildpack/open_jdk_jre/bin/java org.springframework.boot.loader.JarLauncher"
+cf run-job get-app-inventory-scheduled-job
+cf schedule-job get-app-inventory-scheduled-job "0 8 ? * * "
--- destroy.sh
+++ destroy.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-cf delete-job get-app-details-scheduled-job
+cf delete-job get-app-inventory-scheduled-job
 cf delete-service scheduler-for-pcf
-cf delete get-app-details-task
+cf delete get-app-inventory-task
